b5694020a944a2524653af3755de9bf29c550416
rahul_y <parryrpy@gmail.com>
2019-11-07 20:10:49 +0530

    Test page

--- student/models.py
+++ student/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    user_id = models.IntegerField()
+    username = models.CharField(max_length=100 , null=False)
+
+
+class Analysis(models.Model):
+    user_id = models.IntegerField()
+    sub_id = models.IntegerField()
+    topic_id = models.IntegerField()
+    mot_id = models.IntegerField()
+    confidence = models.IntegerField()
+    confidence_s = models.IntegerField()
+    que_id = models.IntegerField()
+    correct = models.IntegerField()
+    attempt = models.IntegerField()
+    time = models.IntegerField()
+    last_time = models.IntegerField()
+    best_time = models.IntegerField()
+    effort = models.IntegerField()
+
+class Subject(models.Model):
+    sub_id = models.IntegerField()
+    sub_name = models.CharField(max_length=1000 , null=False)
+    mots_id = models.IntegerField()
+    confidence_s = models.IntegerField()
+
+class topic(models.Model):
+    top_id = models.IntegerField()
+    top_name = models.CharField(max_length=1000 , null=False)
+    level = models.IntegerField()
+
+class Motivation(models.Model):
+    mot_id = models.IntegerField()
+    mot_quote = models.CharField(max_length=1000 , null=False)
+    mots_id = models.IntegerField()
+
+class Quetions(models.Model):
+    que_id = models.IntegerField()
+    question = models.CharField(max_length=1000 , null=False)
+    opt_1 = models.CharField(max_length=1000 , null=False)
+    opt_2 = models.CharField(max_length=1000 , null=False)
+    opt_3 = models.CharField(max_length=1000 , null=False)
+    opt_4 = models.CharField(max_length=1000 , null=False)
+    sub_id = models.IntegerField()
+    top_id = models.IntegerField()
+    hint = models.CharField(max_length=1000 , null=False)
+    level = models.IntegerField()
+    ans = models.IntegerField()
--- student/views.py
+++ student/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.urls import reverse
+from student.models import Quetions
 
 def home(request):
     return render(request,'student/index2.html')
+
+def test(request):
+    ele=Quetions.objects.all()
+    print(ele)
+    q = []
+    h = []
+    o1 = []
+    o2 = []
+    o3 = []
+    o4 = []
+    c = 0
+    qi = []
+    for i in ele:
+        qi.append(i.que_id)
+        q.append(i.question)
+        h.append(i.hint)
+        o1.append(i.opt_1)
+        o2.append(i.opt_2)
+        o3.append(i.opt_3)
+        o4.append(i.opt_4)
+        c = c + 1
+    print(q,h,o1,o2,o3,o4,c,qi)
+    z = zip(q,h,o1,o2,o3,o4,qi)
+    return render(request,'student/test.html',{"z":z})
+
+# "q":q,"h":h."o1":o1,"o2":o2,"o3":o3,"o4":o4
