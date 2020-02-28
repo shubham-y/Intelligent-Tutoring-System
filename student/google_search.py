ca108daa1e07f98a4adace119b87a57106991329
shubham-y <yadav105shubham@gmail.com>
2020-02-28 09:22:41 +0530

    google_search working script

--- student/google_search.py
+++ student/google_search.py
@@ -2,8 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
 driver = webdriver.Chrome()
-print browser.current_url
-driver.get('https://www.google.co.in/search')
+# print browser.
+base = 'https://www.google.com/search?q='
+query = str(input().strip())
+q = query.replace(' ','+')
+
+driver.get(base+q)
 # searchbox = driver.find_element_by_xpath("//input[@type='text']")
 # searchbox.send_keys('Set theory')
 # searchbutton = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[2]/div[2]/div[2]/center/input[1]')
