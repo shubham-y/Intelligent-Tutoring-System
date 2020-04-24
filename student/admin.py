b9a219e500f73054a0c248185b7327519a60bf5e
shubham-y <yadav105shubham@gmail.com>
2020-04-24 14:43:20 +0530

    Learining resource page,admin page & learning test minor ui change

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from student.models import Quetions,Subject,topic,Analysis,Student,Imaged,Forum,Forum_reply,Kc_ana
 # Register your models here.
+admin.site.register(Subject)
+admin.site.register(topic)
+admin.site.register(Analysis)
+admin.site.register(Student)
+admin.site.register(Imaged)
+
+admin.site.register(Quetions)
+admin.site.register(Forum)
+admin.site.register(Forum_reply)
+admin.site.register(Kc_ana)
+
+
+
+
+
+
+
+
+# for model in models:
+#     admin.site.register(model)
